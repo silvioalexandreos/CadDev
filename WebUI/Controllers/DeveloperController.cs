82cefa8a2ec790a29ddefcfbcec20496287ac0eb
Silvio Alexandre <silvio.alexandreos@gmail.com>
2020-10-14 22:52:53 -0300

    Finalização CRUD

--- WebUI/Controllers/DeveloperController.cs
+++ WebUI/Controllers/DeveloperController.cs
@@ -39,9 +39,101 @@ namespace WebUI.Controllers
             }
         }
 
+        [HttpGet]
+        [Route("{id}", Name = "GetWihtId")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                return Ok (await _service.Get(id));
+            }
+            catch (ArgumentException ex)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]Database.Developer developer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await _service.Post(developer);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetWihtId", new { id = result.Id })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] Database.Developer developer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await _service.Put(developer);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException ex)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
         //public IActionResult Index()
         //{
-            
+
         //    return View();
         //}
         //public IActionResult CadastroDesenvolvedor()
@@ -49,7 +141,7 @@ namespace WebUI.Controllers
         //    ViewBag.Listas = new Business.Report.Level().Lista();
         //    return View();
         //}
-        
+
         //[HttpPost]
         //public void SalvarDeveloper(Database.Developer developer)
         //{
