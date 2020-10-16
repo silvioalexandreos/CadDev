282ece30b71cf78d26bf30c5994a2be35d6300a1
Silvio Alexandre <silvio.alexandreos@gmail.com>
2020-10-15 23:26:12 -0300

    Conexão PostgreSql

--- Database/Migrations/20201015041524_Criar TB.Designer.cs
+++ Database/Migrations/20201015041524_Criar TB.Designer.cs
@@ -4,14 +4,16 @@ using CadFuncionario;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Database.Migrations
 {
     [DbContext(typeof(Connection))]
-    partial class ConnectionModelSnapshot : ModelSnapshot
+    [Migration("20201015041524_Criar TB")]
+    partial class CriarTB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
